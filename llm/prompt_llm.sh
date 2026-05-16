26697d9353721a1a2a194fdc7d0181053a182abd
dabrokarol <dabrowskikarol998@gmail.com>
2026-05-16 09:32:04 +0000

    feat: a script for asking prompts

---
+++ llm/prompt_llm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export LLM_SERVICE_URL=$(gcloud run services describe $LLM_SERVICE --region $REGION --format="value(status.url)")
+export ID_TOKEN=$(gcloud auth print-identity-token)
+
+if [ "$#" -eq 1 ]; then
+    MESSAGE=$1
+else
+    MESSAGE="Jak często powinien być mierzony poziom chloru w basenie?"
+fi
+
+
+curl -X POST "$LLM_SERVICE_URL/api/chat" \
+    -H "Authorization: Bearer $ID_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d "{
+        \"model\": \"SpeakLeash/bielik-4.5b-v3.0-instruct:Q8_0\",
+        \"messages\": [{ \"role\": \"user\", \"content\": \"$MESSAGE\" }],
+        \"stream\": false
+    }"
